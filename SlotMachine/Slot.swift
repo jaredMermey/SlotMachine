f212f79a49b9b4e3884c9f3e0ece99ec57ae5ec7
Jared Mermey <jaredmermey@Jareds-MacBook-Air.local>
2014-10-16 18:53:25 -0400

    slot machine app

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Jared Mermey on 10/15/14.
+//  Copyright (c) 2014 Jared Mermey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
